255dbc5c636e25925f18aa5e8894480f074e89f2
kaka <2571968126@qq.com>
2023-11-09 12:45:18 +0800

    实现对房屋信息的展示，包括对空数据的过滤，不展示

--- src/com/kaka/houserent/view/HouseView.java
+++ src/com/kaka/houserent/view/HouseView.java
@@ -1,10 +1,25 @@
 package com.kaka.houserent.view;
 
+import com.kaka.houserent.domain.House;
+import com.kaka.houserent.service.HouseService;
 import com.kaka.houserent.utils.Utility;
 
 public class HouseView {
     private boolean loop = true;//控制菜单显示
     private char key = ' ';//接收用户输入选择
+    private HouseService houseService = new HouseService(10);
+    public void listHouse(){
+        System.out.println("======================房屋列表====================");
+        System.out.println("编号\t\t房主\t\t电话\t\t地址\t\t月租\t\t\t状态");
+        House[] houses = houseService.list();
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i] == null){
+                break;
+            }
+            System.out.println(houses[i] );
+        }
+        System.out.println("================================================\n");
+    }
     //界面方法，显示主菜单
     public void mainMeun(){
         do {
@@ -31,7 +46,7 @@ public class HouseView {
                     System.out.println("修改");
                     break;
                 case '5':
-                    System.out.println("显示");
+                    listHouse();
                     break;
                 case '6':
                     System.out.println("退出");
