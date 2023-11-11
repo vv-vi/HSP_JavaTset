686f58b436b3af82a8f9d7848d427b3ef96189fa
kaka <2571968126@qq.com>
2023-11-11 16:28:47 +0800

    main方法的特别说明的补充

--- src/com/kaka/main_/Main01.java
+++ src/com/kaka/main_/Main01.java
@@ -2,8 +2,9 @@ package com.kaka.main_;
 
 public class Main01 {
 
-    public static String name;
+    public static String name = "kaka";
 
+    public int n1 = 10000;
 
     public static void hi(){
 
@@ -15,6 +16,8 @@ public class Main01 {
     public static void main(String[] args) {
         //可以直接调用静态属性，name
         System.out.println("name=" + name);
+        //n1非静态的属性，所以直接调用是不行的
+        //System.out.println("n1=" + n1);
         //也可以直接调用静态方法hi（）
         hi();
         //由于cry方法不是静态的，所以不能直接调用，只能通过实例化对象来调用
